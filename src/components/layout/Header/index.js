e1bf71ab04f091b86e93778666956d2c333169f0
Ameya Deokule <deokuleameya@gmail.com>
2021-06-22 18:25:09 +0530

    cleaned up the project and started Header

---
+++ src/components/layout/Header/index.js
@@ -0,0 +1,17 @@
+export const Header = () => {
+	return (
+		<header className='header' data-testid='header'>
+			<nav>
+				<div className='logo'>
+					<img src='/images/logo.png' alt='logo' />
+				</div>
+				<div className='settings'>
+					<ul>
+						<li>+</li>
+						<li>-</li>
+					</ul>
+				</div>
+			</nav>
+		</header>
+	)
+}
